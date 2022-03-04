32b5639638bee59979e728b3597edee8ea3b753e
Nicole Prindle <nprindle18@gmail.com>
2022-03-04 05:05:48 -0500

    Fix variance on Bicontravariant

--- src/main/java/com/github/nprindle/refract/classes/Bicontravariant.java
+++ src/main/java/com/github/nprindle/refract/classes/Bicontravariant.java
@@ -6,17 +6,17 @@ import java.util.function.Function;
 
 public interface Bicontravariant<P extends K2> {
   <A, B, C, D> A2<P, C, D> cimap(
-      final Function<? extends C, ? super A> f,
-      final Function<? extends D, ? super B> g,
+      final Function<? super C, ? extends A> f,
+      final Function<? super D, ? extends B> g,
       final A2<P, A, B> x);
 
   default <A, B, C> A2<P, C, B> cfirst(
-      final Function<? extends C, ? super A> f, final A2<P, A, B> x) {
+      final Function<? super C, ? extends A> f, final A2<P, A, B> x) {
     return cimap(f, y -> y, x);
   }
 
   default <A, B, D> A2<P, A, D> csecond(
-      final Function<? extends D, ? super B> g, final A2<P, A, B> x) {
+      final Function<? super D, ? extends B> g, final A2<P, A, B> x) {
     return cimap(y -> y, g, x);
   }
 }
