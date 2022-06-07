1d2c2bb423d1e586ff9fc357b9e9934160c6ca94
Nicole Prindle <nprindle18@gmail.com>
2022-06-07 14:40:02 -0400

    Add backwards applicative transformation

--- src/main/java/com/github/nprindle/refract/classes/Applicative.java
+++ src/main/java/com/github/nprindle/refract/classes/Applicative.java
@@ -5,7 +5,7 @@ import com.github.nprindle.refract.d17n.K1;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public interface Applicative<Mu extends Applicative.Mu, F extends K1> extends Functor<Mu, F> {
+public interface Applicative<M extends Applicative.Mu, F extends K1> extends Functor<M, F> {
   public static interface Mu extends Functor.Mu {}
 
   public static <Mu extends Applicative.Mu, F extends K1> Applicative<Mu, F> resolve(
@@ -26,5 +26,40 @@ public interface Applicative<Mu extends Applicative.Mu, F extends K1> extends Fu
   default <A, B, C> A1<F, C> apply2(
       final BiFunction<? super A, ? super B, ? extends C> f, final A1<F, A> x, final A1<F, B> y) {
     return ap(map(x2 -> y2 -> f.apply(x2, y2), x), y);
+  }
+
+  default Applicative<M, F> backwards() {
+    final Applicative<M, F> base = this;
+
+    return new Applicative<M, F>() {
+      @Override
+      public <A, B> A1<F, B> map(final Function<? super A, ? extends B> f, final A1<F, A> x) {
+        return base.map(f, x);
+      }
+
+      @Override
+      public <A> A1<F, A> pure(final A x) {
+        return base.pure(x);
+      }
+
+      @Override
+      public <A, B> A1<F, B> ap(final A1<F, Function<? super A, ? extends B>> f, final A1<F, A> x) {
+        return base.apply2((x2, f2) -> f2.apply(x2), x, f);
+      }
+
+      @Override
+      public <A, B, C> A1<F, C> apply2(
+          final BiFunction<? super A, ? super B, ? extends C> f,
+          final A1<F, A> x,
+          final A1<F, B> y) {
+        return base.apply2((b, a) -> f.apply(a, b), y, x);
+      }
+
+      @Override
+      public <A, B, C> BiFunction<A1<F, A>, A1<F, B>, A1<F, C>> lift2(
+          final BiFunction<? super A, ? super B, ? extends C> f) {
+        return (x, y) -> base.apply2((b, a) -> f.apply(a, b), y, x);
+      }
+    };
   }
 }
