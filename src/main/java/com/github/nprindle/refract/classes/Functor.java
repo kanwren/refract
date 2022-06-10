7cc42a7464ca0bb7e8acfef7e5502f554d5439e4
Nicole Prindle <nprindle18@gmail.com>
2022-06-10 15:33:00 -0400

    Add traverseOf_ and backwards folds

--- src/main/java/com/github/nprindle/refract/classes/Functor.java
+++ src/main/java/com/github/nprindle/refract/classes/Functor.java
@@ -3,6 +3,7 @@ package com.github.nprindle.refract.classes;
 import com.github.nprindle.refract.d17n.A1;
 import com.github.nprindle.refract.d17n.C1;
 import com.github.nprindle.refract.d17n.K1;
+import com.github.nprindle.refract.data.Unit;
 import java.util.function.Function;
 
 // F is the Mu of the type the Functor is being implemented for
@@ -14,4 +15,8 @@ public interface Functor<Mu extends Functor.Mu, F extends K1> extends C1<Mu, F> 
   }
 
   <A, B> A1<F, B> map(final Function<? super A, ? extends B> f, final A1<F, A> x);
+
+  default <A> A1<F, Unit> ignore(final A1<F, A> x) {
+    return this.map(a -> Unit.UNIT, x);
+  }
 }
--- src/main/java/com/github/nprindle/refract/optics/Fold.java
+++ src/main/java/com/github/nprindle/refract/optics/Fold.java
@@ -1,5 +1,6 @@
 package com.github.nprindle.refract.optics;
 
+import com.github.nprindle.refract.classes.Applicative;
 import com.github.nprindle.refract.classes.Foldable;
 import com.github.nprindle.refract.classes.Folding;
 import com.github.nprindle.refract.classes.Monoid;
@@ -33,7 +34,7 @@ public interface Fold<S, A> extends Optic<Folding.Mu, S, S, A, A> {
       public <P extends K2> A2<P, S, S> runOptic(
           final A1<? extends Folding.Mu, P> dict, final A2<P, A, A> rel) {
         final Folding<? extends Folding.Mu, P> folding = Folding.resolve(dict);
-        return folding.csecond(a -> Unit.UNIT, folding.wander(wander, rel));
+        return folding.rphantom(folding.wander(wander, rel));
       }
     };
   }
@@ -57,6 +58,18 @@ public interface Fold<S, A> extends Optic<Folding.Mu, S, S, A, A> {
     return this.foldMapOf(Foldr.Instances.monoid(), Foldr::singleton, s).runFoldr(f::apply, z);
   }
 
+  // TODO: optimize this. foldMapOf is more efficient, but then we can't have
+  // the function return F<R>, since making that many .ignore calls may be
+  // expensive. maybe offer two versions of the function, one with a .ignore
+  // and one without (with an `f :: a -> f ()`)?
+  default <F extends K1, R> A1<F, Unit> traverseOf_(
+      final Applicative<? extends Applicative.Mu, F> applicative,
+      final Function<? super A, ? extends A1<F, R>> f,
+      final S s) {
+    return this.foldrOf(
+        (a, rest) -> applicative.before(f.apply(a), rest), applicative.pure(Unit.UNIT), s);
+  }
+
   default Optional<A> preview(final S s) {
     return this.foldMapOf(Monoid.Instances.lazyFirst(), a -> Lazy.of(Optional.of(a)), s).get();
   }
@@ -69,10 +82,22 @@ public interface Fold<S, A> extends Optic<Folding.Mu, S, S, A, A> {
     return this.foldMapOf(Monoid.Instances.intSum(), a -> 1, s);
   }
 
+  default Fold<S, A> backwards() {
+    final Fold<S, A> base = this;
+    return Fold.folding(
+        new Traversing.Wander<S, Unit, A, A>() {
+          @Override
+          public <F extends K1> A1<F, Unit> runWander(
+              final Applicative<? extends Applicative.Mu, F> applicative,
+              final Function<A, A1<F, A>> f,
+              final S s) {
+            return base.traverseOf_(applicative.backwards(), f, s);
+          }
+        });
+  }
+
   // TODO:
-  // traverseOf_ :: Applicative f => Fold s a -> (a -> f r) -> s -> f ()
   // toListOf :: Fold s a -> s -> [a]
-  // foldBackwards :: Fold s a -> Fold s a
 }
 
 // newtype Foldr a = Foldr { runCons :: forall r. (a -> r -> r) -> r -> r }
